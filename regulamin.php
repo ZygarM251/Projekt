b6ddab5111f411259c4e01525eeeb73fca4a83d4
kamilhubgit <116435176+kamilhubgit@users.noreply.github.com>
2023-01-06 13:05:21 +0100

    Regulamin

---
+++ regulamin.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+	<title>Moto-oto Projekt</title>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style5.css">
+</head>
+<body>
+	<div id="header">
+		<div id="menu">
+			<div id="menu-left">
+			Moto-Oto
+			</div>
+			<div id="menu-right">
+				<ul>
+					<li><a href="index.php">Strona Główna</a></li>
+					<li><a href="sell.php">Sprzedaj</a></li>
+					<li><a href="rej.php">Rejestracja</a></li>
+					<li><a href="login.php">Zaloguj</a></li>
+				</ul>
+			</div>
+		</div>
+			<div id="menu-banner">
+				<div id="message">
+				<span>Volvo xc60<br /></span>
+				<p>Volvo xc60 już od 180 tys zł</p>
+				</div>
+                <a href="post.php">
+			<div class="button">
+			OGŁOSZENIA
+            </div>
+            </a>
+			</div>
+</div>
+<div id="right">
+			<div class="news">
+			<h1>REGULAMIN</h1>
+			<p>
+			1. W Serwisie masz możliwość zawarcia z 321Sprzedane umowy sprzedaży
+			Pojazdu, a także umów związanych z Usługami Dodatkowymi, w tym umów
+			o produkty ubezpieczeniowe dotyczące Pojazdu. W Serwisie znajdziesz
+			również informacje na temat możliwości finansowania zakupu Pojazdu
+			w postaci umowy kredytu lub leasingu. 321Sprzedane nie jest stroną umów
+			kredytowych, umów leasingu lub umów ubezpieczenia, a także nie
+			pośredniczy w ich zawieraniu, z wyjątkiem umowy Ubezpieczenia od
+			mechanicznych i elektrycznych awarii w pojazdach silnikowych “Przedłużona
+			Gwarancja” w jednym z wariantów odpłatnych, których stroną (jako zakład
+			ubezpieczeń) jest Fortegra Europe Insurance Company Ltd z siedzibą
+			w Office 13, SOHO Office, The Strand, Fawwara Building, Triq L-Imsida,
+			Gzira, GZR 1401, MALTA.<br>
+			2. Treści zamieszczone w Serwisie mają charakter informacyjny i nie stanowią
+			Oferty w rozumieniu art. 66 i 66(1) kodeksu cywilnego.<br>
+			3. Przeniesienie własności pojazdu odbywa się w momencie podpisania
+			protokołu odbioru pojazdu w punkcie partnerskim lub poprzez potwierdzenie
+			odbioru pojazdu w liście przewozowym w przypadku dostawy bezpośredniej.<br>
+			4. Umowa sprzedaży zostaje zawarta za pomocą środków porozumiewania się
+			na odległość.<br>
+			5. Chcąc zawrzeć umowę sprzedaży musisz najpierw złożyć wniosek
+			o sprawdzenie dostępności Pojazdu, pozostawiając Nam wskazane
+			w formularzu obowiązkowe dane i akceptując niniejszy Regulamin
+			i zapoznając się z Polityką Prywatności.<br>
+			6. Po sprawdzeniu dostępności Pojazdu i wyborze opcji finansowania: PRZELEW,
+			masz możliwość złożenia wniosku o zawarcie umowy sprzedaży, w którym
+			musisz: a) podać wszystkie dane, jakie wymagamy od Ciebie w tym procesie
+			(dane te możemy od Ciebie otrzymać również wcześniej np. w rozmowie
+			telefonicznej), b) zaakceptować Regulamin oraz inne klauzule wymagane<br>
+			w trakcie rejestracji.
+			7. Wszelkie informacje przekazywane Nam przed zawarciem umowy sprzedaży
+			i w trakcie jej obowiązywania powinny być zgodne z prawdą.<br>
+			8. Następnie możesz wybrać jedną z dostępnych Usług Dodatkowych.<br>
+			9. Po dokonaniu wyboru przez Ciebie Pojazdu i wszelkich Usług Dodatkowych,
+			przejdziesz do podsumowania, gdzie będziesz miał możliwość weryfikacji
+			wszystkich danych, wybranych Usług Dodatkowych, zapoznania się z treścią
+			umowy sprzedaży oraz umów dotyczących Usług Dodatkowych (jeśli
+			zdecydujesz się zawrzeć jedną z nich) oraz w razie konieczności ich
+			modyfikacji.<br>
+			10. Następnie otrzymasz od nas maila z umową sprzedaży, umowami
+			dotyczącymi Usług Dodatkowych (jeżeli zdecydujesz się zawrzeć jedna z nich)
+			oraz innymi dokumentami koniecznymi do skutecznego zawarcia ww. umowy
+			(umów). Umowa (umowy) będą uważane za zawarte w momencie, kiedy
+			w e-mailu zwrotnym potwierdzisz Nam, że wyrażasz wolę ich zawarcia.<br>
+			11. Otrzymasz od nas na maila fakturę pro forma, którą musisz opłacić
+			w terminie oznaczonym w dokumencie.<br>
+			12. Po uiszczeniu zapłaty ceny otrzymasz od Nas w wiadomości przesłanej na
+			Twój adres e-mail, informację potwierdzającą zawarcie umowy sprzedaży
+			(oraz Usług Dodatkowych w przypadku ich wyboru), umowę sprzedaży wraz
+			z fakturą VAT.<br>
+			13. Zawarcie umowy sprzedaży nie wymaga podpisania dokumentu umowy.<br>
+			14. Tak, jest taka możliwość poprzez zawarcie umowy kredytu z jedną
+			z instytucji współpracujących z Carsmile. W takiej sytuacji, wybierając
+			interesujący Ciebie Pojazd musisz wybrać opcję finansowania KREDYT oraz
+			określić wysokość wkładu własnego.<br>
+			15. 321Sprzedane nie jest podmiotem udzielającym kredytu ani nie pośredniczy
+			w zawarciu umowy kredytowej. Czynności przygotowawcze i proces zawarcia
+			umowy kredytowej obsługuje Carsmile jako pośrednik kredytu
+			konsumenckiego. Proces zawarcia umowy sprzedaży z opcją finansowania
+			KREDYT odbywa się analogicznie jak w punktach 3-12 z zastrzeżeniami
+			wskazanymi poniżej.<br>
+			16. Po złożeniu wniosku o zawarcie umowy otrzymasz informację czy Twój
+			wniosek o zawarcie umowy kredytu został rozpatrzony pozytywnie.<br>
+			17. Zawarcie umowy sprzedaży nastąpi w momencie uiszczenia przez Ciebie
+			wkładu własnego oraz zawarcia umowy kredytu.<br>
+			18. Umowę kredytu możesz zawrzeć z jedną z instytucji z jaką współpracuje
+			Carsmile: AS Inbank S.A. Oddział w Polsce, Cofidis S.A., Smartney Sp. z o.o,
+			BNP Paribas Bank Polska S.A.<br>
+			19. Jeśli jesteś przedsiębiorcą, możesz także sfinansować zakup auta, zawierając
+			umowę leasingu. 321Sprzedane nie jest podmiotem finansującym leasing ani
+			nie pośredniczy w zawarciu umowy leasingowej. Czynności przygotowawcze
+			i proces zawarcia umowy leasingu obsługuje Carsmile.<br>
+			20. Chcąc zawrzeć umowę leasingu musisz najpierw złożyć wniosek
+			o sprawdzenie dostępności Pojazdu, pozostawiając Nam wskazane dane.<br>
+			21. Po sprawdzeniu dostępności Pojazdu i wyborze opcji finansowania: LEASING,
+			następnie składasz wniosek o zawarcie umowy, w którym musisz:<br>
+			a) podać wszystkie dane, jakie wymagamy od Ciebie w tym procesie (dane
+			te możemy od Ciebie otrzymać również wcześniej np. w rozmowie
+			telefonicznej),<br>
+			b) wskazać wysokość wkładu własnego,<br>
+			c) zaakceptować Regulamin oraz inne klauzule wymagane w trakcie
+			rejestracji.<br>
+			22. Po zweryfikowaniu Twojej zdolności oraz pozytywnego rozpatrzenia wniosku
+			o zawarcie umowy leasingu, otrzymasz na maila decyzję wstępną
+			o przyznaniu Ci finansowania (Decyzja wstępna). Otrzymanie Decyzji
+			wstępnej nie stanowi jeszcze potwierdzenia zawarcia umowy leasingu.<br>
+			23. Następnie Carsmile skontaktuje się z Tobą telefonicznie w celu dopełnienia
+			dalszych formalności w celu zawarcia umowy leasingu, w tym w szczególności
+			jakie informacje i dokumenty musisz przygotować.<br>
+			24. Umowę leasingu zawrzesz z podmiotem finansującym, z jakim współpracuje
+			Carsmile: AS Inbank S.A. Oddział w Polsce, BNP Paribas Bank Polska S.A.,
+			Cofidis S.A., IMPULS LEASING Polska Sp. z o.o., PKO Leasing S.A., Santander
+			Bank Polska S.A., Alior Leasing Sp. z o.o.<br>
+			</p>
+			</div>
+		</div>
+<div id="footer">
+Wszelkie prawa zastrzeżone - &copy; 2022
+</div>
+</html>
