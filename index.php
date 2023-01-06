fa137085f6e2591357eb6967ad6fc96a699546be
KubaWojtak <116113776+KubaWojtak@users.noreply.github.com>
2023-01-06 13:00:15 +0100

    index

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+	<title>Moto-oto Projekt</title>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div id="header">
+		<div id="menu">
+			<div id="menu-left">
+			Moto-Oto
+			</div>
+			<div id="menu-right">
+				<ul>
+					<li><a href="index.php">Strona Główna</a></li>
+					<li><a href="sell.php">Sprzedaj</a></li>
+					<li><a href="rej.php">Rejestracja</a></li>
+					<li><a href="login.php">Zaloguj</a></li>
+				</ul>
+			</div>
+		</div>
+			<div id="menu-banner">
+				<div id="message">
+				<span>Toyota Supra mk4<br /></span>
+				<p>Toyota Supra już od 150 tys zł</p>
+				</div>
+                <a href="post.php">
+			<div class="button">
+			OGŁOSZENIA
+            </div>
+            </a>
+			</div>
+</div>
+<h1>O Nas</h1>
+		<p>Jesteśmy serwisem pomagającym sprzedawać i kupować samochody zarówno nowe jak i używane.</p>
+		<a href="regulamin.php">
+			<div class="button">
+			REGULAMIN
+            </div>
+            </a>
+		</div>	
+<div id="right">
+			<div class="news">
+			<h1>Obsługa klienta</h1>
+			<p>
+			+48 61 880 32 21<br>
+			pomoc@otomoto.pl<br>
+			(od pn. do pt. w godz. 08:00 - 17:00)	
+			</p>
+			</div>
+			<div class="news">
+			<h1>Partnerzy</h1>
+			<p>
+			<a href="https://www.olx.pl/">OLX.pl</a><br>
+			<a href="https://www.otodom.pl/">Otodom.pl</a><br>
+			<a href="https://fixly.pl/">Fixly.pl</a>
+			</p>
+			</div>
+			<div class="news">
+			<h1>Nasze Social Media</h1>
+			<p>
+			<a href="https://www.youtube.com/user/otomotopl">Youtube</a><br>
+			<a href="https://www.instagram.com/otomotopl/">Instagram</a><br>
+			<a href="https://www.facebook.com/otomotopl">Facebook</a>
+			</p>
+			</div>
+			<div class="news">
+			<h1>Logowanie i rejestracja</h1>
+			<li><a href="login.php">Zaloguj</a></li><br>
+			<li><a href="rej.php">Rejestracja</a></li>		
+			</div>
+		</div>
+<div id="footer">
+Wszelkie prawa zastrzeżone - &copy; 2022
+</div>
+</html>
