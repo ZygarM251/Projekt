c75552783830dbbf97af72e5b5848a9132ae2c88
ZygarM251 <115568775+ZygarM251@users.noreply.github.com>
2023-01-09 20:19:56 +0100

    mustlog

---
+++ mustlog.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+	<title>Moto-oto Projekt</title>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style2.css">
+</head>
+<body>
+	<div id="header">
+		<div id="menu">
+			<div id="menu-left">
+			Moto-Oto
+			</div>
+			<div id="menu-right">
+				<ul>
+					<li><a href="index.php">Strona Główna</a></li>
+					<li><a href="sell.php">Sprzedaj</a></li>
+					<li><a href="rej.php">Rejestracja</a></li>
+					<li><a href="login.php">Zaloguj</a></li>
+				</ul>
+			</div>
+		</div>
+			<div id="menu-banner">
+				<div id="message">
+				<span>Peugeot 308<br /></span>
+				<p>Peugeot 308 już od 100 tys zł</p>
+				</div>
+                <a href="post.php">
+			<div class="button">
+			OGŁOSZENIA
+            </div>
+            </a>
+			</div>
+</div>
+<div id=formularz>
+<h2>Musisz się zalogować</h2><br>
+<a href="login.php">Przejdź do logowania</a><br><br>Lub<br><br>
+<a href="rej.php"> Zarejestruj się</a><br><br><br>  
+</div>
+<div id="footer">
+Wszelkie prawa zastrzeżone - &copy; 2022
+</div>
+</html>
